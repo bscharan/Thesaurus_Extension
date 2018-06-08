80e25b8309b0962904bb7236d101d3f943611d00
SAICHARAN <bscharan1729@gmail.com>
2018-06-08 11:44:13 +0530

    Update ContentScript.js

--- ContentScript.js
+++ ContentScript.js
@@ -1,1 +1,20 @@
 // content.js
+
+<script type="text/javascript"></p>
+<p>// Get user selection text on page
+function getSelectedText() {
+    if (window.getSelection) {
+        return window.getSelection();
+    }
+    else if (document.selection) {
+        return document.selection.createRange().text;
+    }
+    return '';
+}</p>
+<p>$(document).ready(function(){
+		$("#button").click(function(){
+			var myObj = $('#highlight');//change to the location of the text you want selected/highlight-able
+			myObj.html( myObj.html().replace(getSelectedText(),'<span class="selected">'+getSelectedText()+'</span>') );//notice class name "selected"
+		});
+});</p>
+<p></script>
